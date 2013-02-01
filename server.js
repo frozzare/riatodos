2c58229b9a33c1ee96be24a6b0344c2ce6203a52
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2013-02-01 10:45:57 +0100

    Adding connect server and moving files and adding some structure

---
+++ server.js
@@ -0,0 +1,14 @@
+var connect = require('connect')
+  , http = require('http')
+  , fs   = require('fs')
+  , app = connect()
+      .use(connect.static(__dirname + '/public/'))
+      .use(function (req, res) {
+        var file = __dirname + (req.originalUrl === '/' ? '/index.html' : req.originalUrl);
+        fs.readFile(file, function (err, buf) {
+          if (!err) res.end(buf.toString());
+        });
+      });
+
+http.createServer(app).listen(3000);
+console.log('Server running at http://localhost:3000');
